17106cc345a0f793b9e10ff3b14e4979183e5518
eduard <120855690+Eduard0110@users.noreply.github.com>
2023-10-30 17:26:09 +0000

    Update object.cpp

--- source/object.cpp
+++ source/object.cpp
@@ -1,39 +1,15 @@
 #include "object.h"
 
-void Object::set_col(float r, float g, float b) {
-    colour[0] = r;
-    colour[1] = g;
-    colour[2] = b;
-}
-
-void Object::set_pos(float x, float y, float z) {
-    pos[0] = x;
-    pos[1] = y;
-    pos[2] = z;
-}
-
-void Object::set_rot(float x, float y, float z) {
-    rot[0] = x;
-    rot[1] = y;
-    rot[2] = z;
-}
-
-void Object::set_cubeSize(float x, float y, float z) {
-    cubeSize[0] = x;
-    cubeSize[1] = y;
-    cubeSize[2] = z;
-}
-
 void Object::set_default_settings(int objType) {
-    if (objType == 0) {
+    if (objType == 0) { // Rought material
         isLight = 0.0;
         refractionIndex = -1.0;
     }
-    else if (objType == 1) {
+    else if (objType == 1) { // Light source
         isLight = 1.0;
         refractionIndex = -1.0;
     }
-    else if (objType == 2) {
+    else if (objType == 2) { // Lens
         isLight = 0.0;
         if (refractionIndex == -1.0) refractionIndex = 1.0;
     }
@@ -41,12 +17,29 @@ void Object::set_default_settings(int objType) {
 
 void Object::set_default_settingsALL() {
     name.clear();
-    set_pos(0.0f, 0.0f, 0.0f);
-    set_rot(0.0f, 0.0f, 0.0f);
-    set_col(1.0f, 1.0f, 1.0f);
+    // pos
+    pos[0] = 0.0f;
+    pos[1] = 0.0f;
+    pos[2] = 0.0f;
+    // rotation
+    rot[0] = 0.0f;
+    rot[1] = 0.0f;
+    rot[2] = 0.0f;
+    // colour
+    colour[0] = 1.0f;
+    colour[1] = 1.0f;
+    colour[2] = 1.0f;
+    // specular colour
+    specularColour[0] = 1.0f;
+    specularColour[1] = 1.0f;
+    specularColour[2] = 1.0f;
+    // cube size
+    cubeSize[0] = 1.0f;
+    cubeSize[1] = 1.0f;
+    cubeSize[2] = 1.0f;
+    // other
     type = 0.0;
     radius = 1.0;
-    set_cubeSize(1.0f, 1.0f, 1.0f);
     isLight = 0.0;
     powerOfLight = 1.0;
     reflectivity = 0.0;
